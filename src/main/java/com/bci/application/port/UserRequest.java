fbd1594b6a8b1e09284105537f770cf6c3eaa152
Luis Ventocilla <lventocilla@codigodelsur.com>
2025-04-18 09:00:41 -0300

    import code

--- src/main/java/com/bci/application/port/UserRequest.java
+++ src/main/java/com/bci/application/port/UserRequest.java
@@ -13,13 +13,15 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class UserRequest {
-    @NotNull
     @NotBlank(message = "El nombre es obligatorio")
+    @NotNull(message = "El nombre no puede ser nulo")
     public String name;
     @Email(message = "El correo no tiene un formato válido")
     @NotBlank(message = "El correo es obligatorio")
+    @NotNull(message = "El correo no puede ser nulo")
     public String email;
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{6,}$", message = "La contraseña no cumple con el formato")
+    @NotNull(message = "La password no puede ser nula")
     public String password;
     @NotNull(message = "La lista de telefonos puede ser vacia pero no nula")
     public List<@Valid PhoneRequest> phones;
